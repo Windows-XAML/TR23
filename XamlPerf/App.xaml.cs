c7778ca749b4552db04c2fc63cc6a207df94bd07
jerrynixon <jerry@nixoncorp.com>
2016-07-25 11:48:31 -0600

    Adding XamPerf

---
+++ XamlPerf/App.xaml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace XamlPerf
+{
+    /// Documentation on APIs used in this page:
+    /// https://github.com/Windows-XAML/Template10/wiki
+
+    [Bindable]
+    sealed partial class App : Template10.Common.BootStrapper
+    {
+        public App() { InitializeComponent(); }
+
+        public override UIElement CreateRootElement(IActivatedEventArgs e) => NavigationServiceFactory(BackButton.Attach, ExistingContent.Include).Frame;
+
+        public override async Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
+        {
+            NavigationService.Navigate(typeof(Views.MainPage));
+            await Task.CompletedTask;
+        }
+    }
+}
+
---
+++ XamlPerf/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+using Template10.Services.NavigationService;
+using Windows.UI.Xaml.Navigation;
+
+namespace XamlPerf.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public void GoToMailRelativePanel() { NavigationService.Navigate(typeof(Views.MailRelativePanel)); }
+        public void GoToMailStandard() { NavigationService.Navigate(typeof(Views.MailStandard)); }
+    }
+}
+
