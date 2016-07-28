c1065cacdebd1e11f2680af7c5808487ef8da271
Robert Evans <robertev@microsoft.com>
2016-07-28 11:47:54 -0700

    Cleaning up a bit..

--- XamlPerf/ViewModels/NewsStandardViewModel.cs
+++ XamlPerf/ViewModels/NewsStandardViewModel.cs
@@ -18,69 +18,38 @@ namespace XamlPerf.ViewModels
 {
     public class NewsStandardViewModel : ViewModelBase
     {
+
         private DataService _dataService;
+        private ObservableCollection<NewsService.Article> _originalItems;
+
+        public ObservableCollection<Article> Articles
+        {
+            get { return _originalItems; }
+            set
+            {
+                _originalItems = value;
+                RaisePropertyChanged();
+
+            }
+        }
+
 
         public NewsStandardViewModel()
         {
-            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
-            {
-                Enumerable.Range(1, 10).ForEach(x => Items.Add(DataService.Sample(x)));
-            }
-            else
-            {
-                _dataService = new DataService();
-            }
+            _dataService = new DataService();
+            LoadItems();
+
         }
 
-        #region properties
 
-        private List<NewsService.Article> _originalItems;
-
-        public ObservableCollection<object> Items { get; set; } = new ObservableCollection<object>();
-
-        public NewsService.Article Item
+        public async void LoadItems()
         {
-            get { return null; }
-            set
-            {
-                //if (value != null)
-                //    NavigationService.Navigate(typeof(Views.ArticlePage), value.Id);
-                RaisePropertyChanged();
-            }
+            var list = await _dataService.GetArticlesAsync();
+            Articles = list.ToObservableCollection<NewsService.Article>();
+
         }
 
-        string _FilterString = string.Empty;
-        public string FilterString { get { return _FilterString; } set { Set(ref _FilterString, value); } }
 
-        #endregion
-
-        public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> suspensionState)
-        {
-            // load articles
-            _originalItems = await _dataService.GetArticlesAsync();
-
-            // restore filter string, if any
-            if (suspensionState.ContainsKey(nameof(FilterString)))
-            {
-                FilterString = suspensionState[nameof(FilterString)]?.ToString();
-            }
-            else if (parameter as string != null)
-            {
-                FilterString = parameter?.ToString();
-            }
-
-            // apply initial filter
-            //Filter();
-        }
-
-        public override Task OnNavigatedFromAsync(IDictionary<string, object> suspensionState, bool suspending)
-        {
-            if (suspending)
-            {
-                suspensionState[nameof(FilterString)] = FilterString;
-            }
-            return Task.CompletedTask;
-        }
 
 
 
