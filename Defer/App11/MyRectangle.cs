068cddbb4f6a313df5e013405aaf05db4f2762e5
Jerry Nixon <jerry@nixoncorp.com>
2016-07-29 10:14:37 -0700

    Adding Defer Demo

---
+++ Defer/App11/MyRectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace App11
+{
+    class MyRectangle: Grid
+    {
+        static int counter = 0;
+        public MyRectangle()
+        {
+            Debug.WriteLine($"Created MyRectangle {counter++}");
+            Loaded += MyRectangle_Loaded;
+        }
+
+        private void MyRectangle_Loaded(object sender, RoutedEventArgs e)
+        {
+            Height = 100;
+            Width = 100;
+            Margin = new Thickness(12);
+            Background = new SolidColorBrush(Colors.SteelBlue);
+        }
+    }
+}
